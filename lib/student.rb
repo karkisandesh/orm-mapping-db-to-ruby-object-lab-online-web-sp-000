cc4187d58e0824ccbe206cc970450c1a568c5fff
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 06:35:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
   end
   
   
-   def self.first_X_students_in_grade_10
+   def self.first_X_students_in_grade_10(number)
      sql = <<-SQL
       SELECT * 
       FROM students
@@ -62,7 +62,7 @@ class Student
       LIMIT ? 
      SQL
      
-     DB[:conn].execute(sql, X)
+     DB[:conn].execute(sql, number)
    end
   
   def self.first_student_in_grade_10
