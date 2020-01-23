2e90aa571e169c4a9a1bb8d707e4d5209d20a2a1
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 06:26:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,16 @@ class Student
   end
   
   
-  
+   def self.first_X_students_in_grade_10
+     sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 10 
+      LIMIT X 
+     SQL
+     
+     DB[:conn].execute(sql, grade)
+   end
   
   
   
