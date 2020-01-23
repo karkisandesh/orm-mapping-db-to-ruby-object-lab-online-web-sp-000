5193d4b0c6f410110bd6424ba95ee2bcd047f7e2
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 06:45:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,13 +43,8 @@ class Student
     DB[:conn].execute(sql)  
   end
  
-  def self.students_below_12th_grade
-    sql = <<-SQL
-      SELECT * 
-      FROM students 
-      WHERE grade < 12
-    SQL
-    
+   def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
     DB[:conn].execute(sql)
   end
   
